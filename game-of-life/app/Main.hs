ebdb68786f22a30cdc730a87318e1e6b55dc35ca
Pavel Anpin <pavel@anpin.fyi>
2025-10-07 07:19:20 -0300

    stateful grid is shown with each cell reacting to user input

--- game-of-life/app/Main.hs
+++ game-of-life/app/Main.hs
@@ -1,40 +1,126 @@
+-- port of hyperlith game-of-life https://github.com/andersmurphy/hyperlith/blob/master/examples/game_of_life/src/app/main.clj
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DeriveAnyClass #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE UndecidableInstances #-}
+{-# OPTIONS_GHC -Wno-unused-imports #-}
+{-# OPTIONS_GHC -Wno-unused-matches #-}
 
 module Main where
 
-import Data.Text (Text)
+import Data.Text as T
 import Web.Atomic.CSS
-import Web.Hyperbole
+import Web.Hyperbole.Effect.OAuth2 (AuthFlow (state))
+
+import Control.Concurrent.STM qualified as STM -- this is required to create effectfull monad in IO / main
+import Control.Lens
+import Control.Monad (forM_, zipWithM_)
+import Data.Text (pack)
+import Effectful
+import Effectful.Concurrent.STM
+import Effectful.Labeled.State (modify)
+import Effectful.Reader.Dynamic
+import Web.Hyperbole as Hyperbole
+
+-- import Data.Foldable (forM_)
+
+port :: Int
+port = 3000
+boardSize :: Int
+boardSize = 50
+_s :: Length
+_s = PxRem 20
+black :: HexColor
+black = HexColor "#000FA"
 
 main :: IO ()
 main = do
   putStrLn $ "Listening on port: " <> show port
+  board <- STM.newTVarIO defaultState
   run port $ do
-    liveApp quickStartDocument (runPage page)
+    -- liveApp quickStartDocument (runPage page)
+    liveApp quickStartDocument (runReader board . runConcurrent $ runPage page)
 
-port :: Int
-port = 3000
-
-page :: (Hyperbole :> es) => Page es '[Message]
+page :: (Hyperbole :> es, Concurrent :> es, Reader (TVar GameState) :> es) => Page es '[GamePage]
 page = do
+  board <- getState
   pure $ do
-    hyper Message1 ~ bold $ messageView "Hello"
-    hyper Message2 ~ bold $ messageView "World!"
+    el ~ bold . fontSize 24 $
+      "Game of Life"
+    hyper GamePage $ boardView board
 
-data Message = Message1 | Message2
+-- hyper board ~ bold $ messageView "Hello"
+
+-- hyper Message2 ~ bold $ messageView "World!"
+
+data Color = Red | Green | Blue
+  deriving (Show, Read, Eq, Generic, ToJSON, FromJSON, ToParam, FromParam)
+
+instance ToColor Color where
+  colorValue Red = "#FF0000"
+  colorValue Green = "#00FF00"
+  colorValue Blue = "#0000FF"
+
+data Cell = Dead | Alive Color
+  -- deriving (Show, Generic, ViewId)
+  deriving (Show)
+toggleCell :: Cell -> Cell
+toggleCell Dead = Alive Red
+toggleCell (Alive c) = Dead
+type GameState = [[Cell]]
+
+data GamePage = GamePage -- this is like a ViewModel binding in MVVM? Seems like it dosn't need to have an actual value
   deriving (Generic, ViewId)
 
-instance HyperView Message es where
-  data Action Message = Louder Text
+-- instance Default GameState where
+--   def = [[Dead | _ <- [1 .. boardSize]] | _ <- [1 .. boardSize]]
+defaultState :: GameState
+defaultState = [[Dead | _ <- [1 .. boardSize]] | _ <- [1 .. boardSize]]
+
+getState :: (Concurrent :> es, Reader (TVar GameState) :> es) => Eff es GameState
+getState = readTVarIO =<< ask
+
+modifyState :: (Concurrent :> es, Reader (TVar GameState) :> es) => (GameState -> GameState) -> Eff es GameState
+modifyState f = do
+  var <- ask
+  atomically $ do
+    modifyTVar var f
+    readTVar var
+
+toggleCellAt :: Int -> Int -> GameState -> GameState
+toggleCellAt x y =
+  -- let newCell = toggleCell $ board !! x !! y
+  ix y . ix x %~ toggleCell
+
+instance (Reader (TVar GameState) :> es, Concurrent :> es) => HyperView GamePage es where
+  data Action GamePage = TapCell Int Int
     deriving (Generic, ViewAction)
 
-  update (Louder msg) = do
-    let new = msg <> "!"
-    pure $ messageView new
+  update (TapCell x y) = do
+    -- board <- getState -- TODO: update state
+    -- pure $ cellView x y $ toggleCell $ board !! x !! y
+    board <- modifyState (toggleCellAt x y)
+    pure $
+      boardView board
 
-messageView :: Text -> View Message ()
-messageView msg = do
-  button (Louder msg) ~ border 1 $ text msg
+cellView :: Int -> Int -> Cell -> View GamePage ()
+cellView y x cell = do
+  button (TapCell x y)
+    ~ border 1
+    ~ bg (case cell of Dead -> black; Alive c -> colorValue c)
+    ~ height _s
+    ~ width _s
+    $ text
+    $ case cell of
+      Dead -> "X"
+      Alive _ -> "O"
+
+boardView :: GameState -> View GamePage ()
+boardView state = do
+  zipWithM_
+    ( \y r ->
+        zipWithM_ (cellView y) [0 ..] r
+    )
+    [0 ..]
+    state
